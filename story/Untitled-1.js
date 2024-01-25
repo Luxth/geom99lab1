b7607bd92a0aa469fa1d666d9a525c4e13904e64
Luxth <146376027+Luxth@users.noreply.github.com>
2024-01-25 17:37:10 -0500

    Update Untitled-1.js

--- story/Untitled-1.js
+++ story/Untitled-1.js
@@ -8,29 +8,28 @@ async function initMap() {
         zoom: 13,
         center: { lat: 40.7439208, lng: -73.9971402 },
         mapId: "4504f8b37365c3d0",
-      },
-    )
+      });
     
     const roadtrip = [
       { 
-          position: lat: 40.756649, lng: -73.9888153 }, 
-          title = "Hilton New York Times Square Hotel",
+          position: {lat: 40.756649, lng: -73.9888153 }, 
+          title: "Hilton New York Times Square Hotel",
       },    
       { 
-          position: lat: 40.7579747, lng: -73.9855426 },
-          title = "Times Square",
+          position: {lat: 40.7579747, lng: -73.9855426 },
+          title: "Times Square",
       },
       { 
-          position: lat: 40.7484405, lng: -73.9856644 }, 
-          title = "Empire State Building",
+          position: {lat: 40.7484405, lng: -73.9856644 }, 
+          title: "Empire State Building",
       },
       { 
-          position: lat: 40.7443371, lng: -73.9920054 },
-          title = "2 Bros Pizza",
+          position: {lat: 40.7443371, lng: -73.9920054 },
+          title: "2 Bros Pizza",
       },
       { 
-          position: lat: 40.722233, lng: -73.987429 },
-          title = "Katz's Delicatessen",
+          position: {lat: 40.722233, lng: -73.987429 },
+          title: "Katz's Delicatessen",
       },    
     ];
     
