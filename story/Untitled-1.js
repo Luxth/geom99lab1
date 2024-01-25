ccea3756dcbe674928c04bd5d3476c1eaa76df24
Luxth <146376027+Luxth@users.noreply.github.com>
2024-01-25 17:21:02 -0500

    Update Untitled-1.js

--- story/Untitled-1.js
+++ story/Untitled-1.js
@@ -1,4 +1,7 @@
 async function initMap() {
+    const { Map, InfoWindow } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+    const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
+    
     const map = new google.maps.Map
      ( document.getElementById("map"),
       {
