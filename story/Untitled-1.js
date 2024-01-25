b2debdb775aaabf6827070e547c18f68d0d85d8d
Luxth <146376027+Luxth@users.noreply.github.com>
2024-01-25 17:52:40 -0500

    Update Untitled-1.js

--- story/Untitled-1.js
+++ story/Untitled-1.js
@@ -1,60 +1,61 @@
 async function initMap() {
-    const { Map, InfoWindow } = await google.maps.importLibrary("maps");
-    const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary("marker");
     
-    const map = new google.maps.Map
-     ( document.getElementById("map"),
-      {
-        zoom: 13,
-        center: { lat: 40.7439208, lng: -73.9971402 },
-        mapId: "4504f8b37365c3d0",
-      });
+    const { Map, InfoWindow } = await google.maps.importLibrary("maps");
+    const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
+      "marker",
+    );
+    const map = new Map(document.getElementById("map"), {
+      zoom: 12,
+      center: { lat: 40.7439208, lng: -73.9971402 },
+      mapId: "4504f8b37365c3d0",
+    });
     
     const roadtrip = [
-      { 
-          position: {lat: 40.756649, lng: -73.9888153 }, 
-          title: "Hilton New York Times Square Hotel",
-      },    
-      { 
-          position: {lat: 40.7579747, lng: -73.9855426 },
-          title: "Times Square",
+      {
+        position: { lat: 40.756649, lng: -73.9888153 },
+        title: "Hilton New York Times Square Hotel",
       },
-      { 
-          position: {lat: 40.7484405, lng: -73.9856644 }, 
-          title: "Empire State Building",
+      {
+        position: { lat: 40.7579747, lng: -73.9855426 },
+        title: "Times Square",
       },
-      { 
-          position: {lat: 40.7443371, lng: -73.9920054 },
-          title: "2 Bros Pizza",
+      {
+        position: { lat: 40.7484405, lng: -73.9856644 },
+        title: "Empire State Building",
       },
-      { 
-          position: {lat: 40.722233, lng: -73.987429 },
-          title: "Katz's Delicatessen",
-      },    
+      {
+        position: { lat: 40.7443371, lng: -73.9920054 },
+        title: "2 Bros Pizza",
+      },
+      {
+        position: { lat: 40.722233, lng: -73.987429 },
+        title: "Katz's Delicatessen",
+      },
     ];
-    
-    
-    
-    // Create an info window to share between markers.
-    const infoWindow = new google.maps.InfoWindow();
+   
+    const infoWindow = new InfoWindow();
   
-    // Create the markers.
-    roadtrip.forEach(([position, title], i) => {
-      const marker = new google.maps.Marker({
+   
+    roadtrip.forEach(({ position, title }, i) => {
+      const pin = new PinElement({
+        glyph: `${i + 1}`,
+      });
+      const marker = new AdvancedMarkerElement({
         position,
         map,
         title: `${i + 1}. ${title}`,
-        label: `${i + 1}`,
-        optimized: false,
+        content: pin.element,
       });
-
   
       // Add a click listener for each marker, and set up the info window.
-      marker.addListener("click", () => {
+      marker.addListener("click", ({ domEvent, latLng }) => {
+        const { target } = domEvent;
+  
         infoWindow.close();
-        infoWindow.setContent(marker.getTitle());
-        infoWindow.open(marker.getMap(), marker);
+        infoWindow.setContent(marker.title);
+        infoWindow.open(marker.map, marker);
       });
     });
-    
   }
+  
+  initMap();
