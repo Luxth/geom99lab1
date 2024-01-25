7110cdee1e73d338975eb05a4f59accf984f7ccc
Luxth <146376027+Luxth@users.noreply.github.com>
2024-01-25 16:26:26 -0500

    Update clustering.js

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -1,7 +1,7 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 3,
-    center: { lat: 50.5386803, lng: -84.7173986 },
+    center: { lat: 47.352455, lng: -81.997528 },
   });
   // Create an array of alphabetical characters used to label the markers.
   const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
