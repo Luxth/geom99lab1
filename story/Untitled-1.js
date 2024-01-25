84be085dfedb9e46e4d6701ecdc33361fbf29efc
Luxth <146376027+Luxth@users.noreply.github.com>
2024-01-25 17:45:52 -0500

    Update Untitled-1.js

--- story/Untitled-1.js
+++ story/Untitled-1.js
@@ -56,17 +56,5 @@ async function initMap() {
         infoWindow.open(marker.getMap(), marker);
       });
     });
-      const newyorkpath = new google.maps.Polyline({
-      path: roadtrip, 
-      geodesic: true,
-      strokeColor: "#FF0000", 
-      strokeOpacity: 1.0, 
-      strokeWeight: 2, 
-    });
-    
-    newyorkpath.setMap(map);
     
   }
-
-  
-  
