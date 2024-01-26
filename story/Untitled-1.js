939fc8a3f61a05cb1d7f204801a0ed2a72c7cae6
Luxth <146376027+Luxth@users.noreply.github.com>
2024-01-26 12:44:37 -0500

    Update Untitled-1.js

--- story/Untitled-1.js
+++ story/Untitled-1.js
@@ -1,3 +1,4 @@
+// Template referenced for the map: https://developers.google.com/maps/documentation/javascript/examples/advanced-markers-accessibility
 async function initMap() {
     
     const { Map, InfoWindow } = await google.maps.importLibrary("maps");
@@ -9,7 +10,7 @@ async function initMap() {
       center: { lat: 40.7439208, lng: -73.9971402 },
       mapId: "4504f8b37365c3d0",
     });
-    
+    // The centre in New York City and the zoom is at city level
     const roadtrip = [
       {
         position: { lat: 40.756649, lng: -73.9888153 },
@@ -23,23 +24,27 @@ async function initMap() {
         position: { lat: 40.7484405, lng: -73.9856644 },
         title: "Empire State Building",
       },
+        // Want to visit the empire state building
       {
         position: { lat: 40.7443371, lng: -73.9920054 },
         title: "2 Bros Pizza",
       },
+        // 2 Bros Pizza is known for the one dollar pizza
       {
         position: { lat: 40.722233, lng: -73.987429 },
         title: "Katz's Delicatessen",
       },
+        // Best place for rye and pastrami in New York at the location called Katz Delicatessen
     ];
-   const myroadpath = [
+   // Template reference for the polylines: https://developers.google.com/maps/documentation/javascript/examples/polyline-simple
+    const myroadpath = [
         {lat: 40.756649, lng: -73.9888153},
         {lat: 40.7579747, lng: -73.9855426},
         {lat: 40.7484405, lng: -73.9856644},
         {lat: 40.7443371, lng: -73.9920054},
         {lat: 40.722233, lng: -73.987429},
     ];
-
+    // Each coordinates in the myroadpath are the points used in the roadtrip constant 
     const myroadtrip = new google.maps.Polyline({
         path: myroadpath,
         geodesic: true,
@@ -47,7 +52,7 @@ async function initMap() {
         strokeOpacity: 1.0,
         strokeWeight: 3,
     });
-
+    // The polylines are transparent and have weight of 3 which is big. Also the polylines are green
     myroadtrip.setMap(map);
     
     const infoWindow = new InfoWindow();
@@ -63,6 +68,7 @@ async function initMap() {
         title: `${i + 1}. ${title}`,
         content: pin.element,
       });
+        // This code is for adding title to the numbered pin
   
       // Add a click listener for each marker, and set up the info window.
       marker.addListener("click", ({ domEvent, latLng }) => {
